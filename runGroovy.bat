91bc99682087502bb0e5dae05361724bb0f75e79
fujiij <32182215+fujiij@users.noreply.github.com>
2018-04-04 11:37:31 +0900

    add runGroovy.bat, edit config.txt

---
+++ runGroovy.bat
@@ -0,0 +1,3 @@
+SET LIB_DIR=.\jamsketch_lib\
+
+groovy -classpath %LIB_DIR%cmx-trunk.jar;%LIB_DIR%core.jar;%LIB_DIR%bsf-2.4.0.jar;%LIB_DIR%commons-logging-1.2.jar;%LIB_DIR%commons-math3-3.6.1.jar;%LIB_DIR%commons-math-2.0.jar;%LIB_DIR%controlP5.jar;%LIB_DIR%groovy-all-2.4.12.jar;%LIB_DIR%ivy-2.4.0.jar;%LIB_DIR%jahmm-0.6.1.jar;%LIB_DIR%javacpp-0.5.jar;%LIB_DIR%javacv.jar;%LIB_DIR%javaoctave-0.6.0.jar;%LIB_DIR%jl1.0.1.jar;%LIB_DIR%servlet-api-2.4.jar;%LIB_DIR%weka-3-6-15.jar;%LIB_DIR%lan.jar;%LIB_DIR%xercesImpl.jar;%LIB_DIR%bluecove-2.1.0.jar;. jamsketch.groovy
